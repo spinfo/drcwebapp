9ae8a5c685bebb6148ce92f57a0bc754cabb5ddf
claesn <c.neuefeind@uni-koeln.de>
2015-11-22 02:47:18 +0100

    Add base for index management.

--- src/main/java/de/uni_koeln/spinfo/drc/controller/LuceneIndexManagerController.java
+++ src/main/java/de/uni_koeln/spinfo/drc/controller/LuceneIndexManagerController.java
@@ -8,62 +8,73 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import de.uni_koeln.spinfo.drc.lucene.Indexer;
 
 @Controller
-public class IndexerController {
-	/*
-	 * TODO Seite mit navbar + entsprechenden Aktionen.
-	 */
+@RequestMapping(value = "/drc")
+public class LuceneIndexManagerController {
+
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private Indexer indexer;
 
+	@RequestMapping(value = "/indexManager")
+	public ModelAndView search() {
+		return new ModelAndView("indexManager");
+	}
+
 	/*
-	 * Indexierung manuell anstoßen.
+	 * Trigger index manually - existing index will be deleted first.
 	 */
 	@RequestMapping("/reindex")
 	@ResponseBody
 	String reindex() {
-		int numDocs = indexer.getNumDocs();
+		logger.info("RE-INDEX");
+		int numDocsBefore = 0;
+		int numDocsAfter = 0;
 		try {
-			if (numDocs > 0) {
-				logger.info("Docs to be deleted: " + numDocs);
+			if (!indexer.isAvailable()) {
+				logger.info("Open indexWriter");
 				indexer.init();
+			}
+			numDocsBefore = indexer.getNumDocs();
+			if (numDocsBefore > 0) {
+				logger.info("Docs to be deleted: " + numDocsBefore);
 				indexer.deleteIndex();
 			}
-			long start = System.currentTimeMillis();
-			indexer.init();
-			logger.info("Indexing took " + (System.currentTimeMillis() - start) + " ms.");
+			indexer.index();
+			numDocsAfter = indexer.getNumDocs();
 			indexer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return "Re-index complete! New index size: " + indexer.getNumDocs() + "(was: " + numDocs + ").";
+		return "Re-index complete! New index size: " + numDocsAfter + "(was: " + numDocsBefore + ").";
 	}
 
 	/*
-	 * Index löschen.
+	 * Delete existing index.
 	 */
-	@RequestMapping("/deleteindex")
+	@RequestMapping("/deleteIndex")
 	@ResponseBody
 	String deleteIndex() {
-		int numDocs = indexer.getNumDocs();
+		logger.info("DELETE INDEX");
+		int numDocs = 0;
 		try {
+			if (!indexer.isAvailable()) {
+				logger.info("Open indexWriter");
+				indexer.init();
+			}
+			numDocs = indexer.getNumDocs();
 			if (numDocs > 0) {
-				logger.info("Docs to be deleted: " + numDocs);
 				indexer.deleteIndex();
-			} else {
-				logger.info("Nothing to delete!");
-				return "Nothing to delete!";
 			}
 			indexer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return "Index deleted!";
+		return numDocs > 0 ? numDocs + " docs deleted!" : "Nothing to delete!";
 	}
-
 }
